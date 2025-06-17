1419908a0ea1b0ed1c63a441db4d73700cd04756
russiancmo <russiancmo@gmail.com>
2025-06-17 16:00:34 +0300

    fix

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -15,7 +15,7 @@ class AuthService {
 
   private constructor() {
     this.keycloakConfig = {
-      url: process.env.baseURLKeycloakFE || "https://ims-mts:8087",
+      url: process.env.baseURLKeycloakFE || "https://ims-mts.ru:8443",
       realm: "ims",
       clientId: "ims_client",
     };
